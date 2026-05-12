e636d91c939ea4e0344e12c3684a2406e18cb1be
samsanaGIT <samsana628@gmail.com>
2026-05-12 09:38:47 +0700

    Done f2

--- index.py
+++ index.py
@@ -3,3 +3,5 @@ num2 = float(input("Enter second number: "))
 
 print("Addition:", num1 + num2)
 print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 * num2)
