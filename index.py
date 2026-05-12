e02575280b33e9d7db842266c60e9ae84caaf9b9
Nom sheivorly <kaynn1266@gmail.com>
2026-05-12 10:51:05 +0700

    Change Done

--- index.py
+++ index.py
@@ -4,4 +4,7 @@ num2 = float(input("Enter second number: "))
 print("Addition:", num1 + num2)
 print("Subtraction:", num1 - num2)
 print("Multiplication:", num1 * num2)
-print("Division:", num1 / num2)
+if num2 != 0:
+    print("Division:", num1 / num2)
+else:
+    print("Cannot divide by zero")
